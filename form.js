fc71aa867b979c279ed32ae169eed5af428fd948
Moritz Biehl <biehlmo@gmail.com>
2024-06-16 12:59:10 +0200

    Export code to Card and Calculate

--- form.js
+++ form.js
@@ -1,60 +1,6 @@
-// For new cards
-const newQuestionCard = document.querySelector('[data-js="form"]');
-// For calculate character
-const amountLeftQuestion = document.querySelector(
-  '[data-js="amountLeftQuestion"]'
-);
-const amountLeftAnswer = document.querySelector('[data-js="amountLeftAnswer"]');
-const questionInput = document.querySelector('[data-js="questionInput"]');
-const answerInput = document.querySelector('[data-js="answerInput"]');
-const maxLengthQuestion = questionInput.getAttribute("maxLength");
-const maxLengthAnswer = answerInput.getAttribute("maxLength");
-// -----------------------------------------------------------------------------------------
+import { Calculate } from "./components/Calculate/Calculate.js";
+import { Card } from "./components/Card/Card.js";
 
-function calculateCharacters(totalCharactersQuestion, totalCharactersAnswer) {
-  amountLeftQuestion.textContent = totalCharactersQuestion;
-  amountLeftAnswer.textContent = totalCharactersAnswer;
-}
+const CalculateElement = Calculate();
 
-newQuestionCard.addEventListener("input", (event) => {
-  calculateCharacters(
-    maxLengthQuestion - questionInput.value.length,
-    maxLengthAnswer - answerInput.value.length
-  );
-});
-// -----------------------------------------------------------------------------------------
-// Create new card and show them below the question cards
-newQuestionCard.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formElements = event.target.elements;
-  const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData);
-  // Create new card
-  const newSection = document.createElement("section");
-  newSection.classList.add("card");
-  newSection.innerHTML = `
-  <h2>${data.questionInput}</h2>
-  <button class="button-bookmark">
-            <i
-              class="fas fa-bookmark fa-2xl card-bookmark toggle-bookmark"
-              data-js="bookmarkIcon"
-            ></i>
-          </button>
-          <div class="button-section">
-            <button class="answer-button" data-js="showAnswerButton">
-              Show Answer
-            </button>
-          </div>
-          <p hidden data-js="answerText">
-            ${data.answerInput}
-          </p>
-          <ul class="tags">
-            <li>${data.tag}</li>
-          </ul>
-  `;
-  {
-  }
-  document.body.append(newSection);
-  event.target.reset();
-  formElements.questionInput.focus();
-});
+const CardElement = Card();
