286ff87c5f809644411868b76b8e596a5a55bfec
Moritz Biehl <biehlmo@gmail.com>
2024-06-16 12:51:00 +0200

    Add class and script module

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const bookmarkIcon = document.querySelector('[data-js="bookmarkIcon"]');
-const showAnswerButton = document.querySelector('[data-js="showAnswerButton"]');
-const answerText = document.querySelector('[data-js="answerText"]');
+import { BookmarkToggle } from "./components/BookmarkToggle/BookmarkToogle.js";
+import { ShowAnswer } from "./components/ShowAnswer/ShowAnswer.js";
 
-// Toggle for card bookmark icon
-bookmarkIcon.addEventListener("click", () => {
-  bookmarkIcon.classList.toggle("toggle-bookmark");
-});
+const BookmarkToggleElement = BookmarkToggle();
 
-// Toggle the answer text and show/hide answer/hide text
-showAnswerButton.addEventListener("click", (event) => {
-  answerText.toggleAttribute("hidden");
-  if (answerText.hasAttribute("hidden")) {
-    showAnswerButton.textContent = "Show Answer";
-  } else {
-    showAnswerButton.textContent = "Hide Answer";
-  }
-});
+const ShowAnswerElement = ShowAnswer();
